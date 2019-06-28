7ebb9225a71db9ddb2439658dd9a89f318e6e395
chamilad <chamila@apache.org>
2019-06-28 20:55:33 +1200

    Implement line count flag
    This change implements a line count flag
    similar to the tail command in bash. The
    file has to be read backwards from the
    end.

--- tail.go
+++ tail.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
-// usage: tailf <filename>
+// usage: tailf <initial line count> <filename>
 func main() {
 	// args without bin name
 	if len(os.Args) == 1 {
@@ -16,17 +18,33 @@ func main() {
 	}
 	args := os.Args[1:]
 
-	fname := args [0]
+	var fname string
+	var lcount int
+
+	// check if a line count is provided
+	if len(args) == 2 {
+		lcount = extractLineCount(args[0])
+		fname = args[1]
+	} else {
+		lcount = 0
+		fname = args[0]
+	}
 	// check if file exists
 	f, err := os.Open(fname)
+	// close the handler later
+	defer f.Close()
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "file not found: %s", fname)
 		os.Exit(1)
 	}
 
+	// if a line count is provided, rewind cursor
+	// the file should be read from the end, backwards
+	f = seekBackwardsToLineCount(lcount, f)
+
+	// wait for new lines to appear
 	reader := bufio.NewReader(f)
 	for {
-
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
@@ -39,6 +57,69 @@ func main() {
 
 		_, _ = fmt.Fprint(os.Stdout, line)
 	}
+}
 
-	os.Exit(0)
+// seekBackwardsToLineCount will move the read position of
+// the passed file until the specified line count from end
+// is met
+// Returns the os.File reference which has a rewound cursor
+func seekBackwardsToLineCount(lc int, f *os.File) *os.File {
+	// line count counter
+	l := 0
+	// offset counter, negative because counting backwards
+	offset := -1
+
+	// loop until lc is passed
+	for ; ; offset-- {
+		// seek backwards by offset from the end
+		p, err := f.Seek(int64(offset), io.SeekEnd)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error while seeking by char at %d: %s", offset, err)
+			return nil
+		}
+
+		// read one char, a new reader is needed from seeked File ref
+		r := bufio.NewReader(f)
+		b, err := r.ReadByte()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error while reading char at %d: %s", p, err)
+			return nil
+		}
+
+		// check if read char is new line
+		s := string(b)
+		if s == "\n" {
+			l++
+			// if line count is passed
+			if l > lc {
+				// increase the offset by one (to compensate for last
+				// read new line
+				offset++
+
+				// escape from loop
+				break
+			}
+		}
+	}
+
+	_, err := f.Seek(int64(offset), io.SeekEnd)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "end: error while seeking by char at %d: %s", offset, err)
+		return nil
+	}
+
+	return f
+}
+
+// extractLineCount parses the given string to a usable int value
+// It can tolerate - prefix
+func extractLineCount(s string) int {
+	s = strings.TrimPrefix(s, "-")
+	s = strings.TrimSpace(s)
+
+	if i, err := strconv.ParseInt(s, 10, 0); err != nil {
+		return 0
+	} else {
+		return int(i)
+	}
 }
