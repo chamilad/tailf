7b5e67ee76c7589d57a3fe9374f9373122aacfca
chamilad <chamila@apache.org>
2019-07-06 12:48:25 +1200

    Modify test script to write to common dir

--- writer.sh
+++ writer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+filename=logs/myserver.$(date +%Y_%m_%d).log
+
 while :
 do
     line=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $((100 + RANDOM % 250)) | head -n 1)
-    echo "$(date): ${line}" >> myserver.$(date +%Y_%m_%d).log
+    echo "$(date): ${line}" >> $filename
     sleep $((0 + RANDOM % 3))
 done
 
